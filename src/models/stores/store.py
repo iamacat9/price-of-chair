868b8ea53cba2e4ac4a28f736ef519bf279db4f3
Xiaoli Shang <XiaoliShang@Xiaolis-iMac.local>
2017-11-16 13:56:09 -0800

    try convert it back

--- src/models/stores/store.py
+++ src/models/stores/store.py
@@ -3,7 +3,6 @@ from src.common.database import Database
 import src.models.stores.constants as StoreConstants
 import src.models.stores.errors as StoreErrors
 
-__author__ = 'jslvtr'
 
 
 class Store(object):
@@ -45,8 +44,19 @@ class Store(object):
         return cls(**Database.find_one(StoreConstants.COLLECTION, {"name": store_name}))
 
     @classmethod
+    def get_by_url_prefix(cls, url_prefix):
+        return cls(**Database.find_one(StoreConstants.COLLECTION, {"url_prefix": {"$regex": '^{}'.format(url_prefix)}}))
+
+    @classmethod
     def find_by_url(cls, url):
-        for store in Database.find(StoreConstants.COLLECTION, {}):
-            if re.match(store['url_prefix'], url):
-                return cls(**store)
-        raise StoreErrors.StoreNotFoundException("The URL prefix used to find the store did not return any results.")
+        """
+        Return a store from a url like "http://www.johnlewis.com/item/sdfj4h5g4g21k.html"
+        :param url: The item's URL
+        :return: a Store, or raises a StoreNotFoundException if no store matches the URL
+        """
+        for i in range(0, len(url)+1):
+            try:
+                store = cls.get_by_url_prefix(url[:i])
+                return store
+            except:
+                raise StoreErrors.StoreNotFoundException("The URL Prefix used to find the store didn't give us any results!")
