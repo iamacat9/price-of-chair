f82f06597ed46dfc6888ebe72616e55ed701ea50
Xiaoli Shang <XiaoliShang@Xiaolis-iMac.local>
2017-11-16 14:01:04 -0800

    modify again

--- src/models/stores/store.py
+++ src/models/stores/store.py
@@ -49,7 +49,14 @@ class Store(object):
 
     @classmethod
     def find_by_url(cls, url):
-        for store in Database.find(StoreConstants.COLLECTION, {}):
-            if re.match(store['url_prefix'], url):
-                return cls(**store)
-        raise StoreErrors.StoreNotFoundException("The URL prefix used to find the store did not return any results.")
+        """
+        Return a store from a url like "http://www.johnlewis.com/item/sdfj4h5g4g21k.html"
+        :param url: The item's URL
+        :return: a Store, or raises a StoreNotFoundException if no store matches the URL
+        """
+        for i in range(0, len(url)+1):
+            try:
+                store = cls.get_by_url_prefix(url[:i])
+                return store
+            except:
+                raise StoreErrors.StoreNotFoundException("The URL Prefix used to find the store didn't give us any results!")
